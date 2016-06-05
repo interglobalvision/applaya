22f2f579d25c21082f72547a7fc3d2e7c8574f59
cas <casska@gmail.com>
2016-06-05 15:02:10 -0500

    add login link to navbar

--- app/imports/ui/navbar.jsx
+++ app/imports/ui/navbar.jsx
@@ -5,6 +5,7 @@ export default Navbar = () => (
   <nav role="navigation">
     <ul>
       <li><a href="/">Home</a></li>
+      <li><a href="/login">Login</a></li>
       <li><a href="#">Navbar Link</a></li>
     </ul>
   </nav>
