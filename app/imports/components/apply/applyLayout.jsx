255660f1c6f9a0796e236effb92a3cbd7cecb13c
cas <casska@gmail.com>
2016-06-08 15:02:17 -0500

    add better dynamic component rendering in applyLayout

--- app/imports/components/apply/applyLayout.jsx
+++ app/imports/components/apply/applyLayout.jsx
@@ -2,28 +2,28 @@ import React, { PropTypes } from 'react';
 
 // Import apply parts
 import TextTest from '/imports/components/apply/text-test.jsx';
-
 import FormTest from '/imports/components/apply/form-test.jsx';
 
-const getApplySection = ( isLogged, user, apply ) => {
+const applySections = {
+  'text-test': <TextTest />,
+  'form-test': <FormTest />,
+};
+
+const getApplySection = ( isLogged, user, section ) => {
   // Check if logged in
   if( !isLogged  ) {
     return <Accounts.ui.LoginForm />;
   }
 
-  if( !!apply.section ) {
-    switch(apply.section) {
-      case 'text-test': return <TextTest />;
-      case 'form-test': return <FormTest />;
-      default: return ':('; // 404
-    };
+  if( !!section ) {
+    return applySections[section];
   }
 
 };
 
 export default ApplyLayout = ({ isLogged, user, apply }) => (
   <section className="applyLayout">
-    { getApplySection(isLogged, user, apply) }
+    { getApplySection(isLogged, user, apply.section) }
   </section>
 );
 
