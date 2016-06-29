b70f79e69962a7c07b9ed32053539bcd23c652ad
cas <casska@gmail.com>
2016-06-29 09:55:52 -0500

    add StepsInfo

--- app/imports/components/apply/steps.js
+++ app/imports/components/apply/steps.js
@@ -1,13 +1,28 @@
 // Import apply parts
 import TextTest from '/imports/components/apply/text-test.jsx';
 import FormTest from '/imports/components/apply/form-test.jsx';
+import GalleryInformation from '/imports/components/apply/galleryInformation.jsx';
 
-const Steps = [{ // 1
+// Import schemas
+import { FormSchema } from '/imports/schemas/TestForm.js';
+import { GalleryInformationSchema } from '/imports/schemas/GalleryInformation.js';
+
+export const Steps = [{ // 1
   component: FormTest,
   name: 'First Form',
+  key: 'form-test',
+  schema: FormSchema
 }, { // 2
   component: TextTest,
   name: 'Text page example',
+  key: 'text-test',
+}, {
+  component: GalleryInformation,
+  name: 'Gallery Information',
+  key: 'gallery-information',
+  schema: GalleryInformationSchema
 }];
 
-export default Steps;
+export const StepsInfo = _.map(Steps, function(step) {
+  return _.omit(step, 'component');
+});
