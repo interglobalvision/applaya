694de156d43338f8d73bbf652fb4924be9219d30
cas <casska@gmail.com>
2016-06-30 23:03:40 -0500

    add artists section

---
+++ app/imports/schemas/artists.js
@@ -0,0 +1,45 @@
+export const ArtistsSchema = new SimpleSchema({
+  // Artists
+  artists: {
+    type: [Object,],
+    minCount: 1,
+  },
+  'artists.$.name': {
+    type: String,
+  },
+  /*
+  * To be implemented
+  'artists.$.cv': {
+    type: String,
+    autoform: {
+      type: "docUpload",
+    },
+  },
+  */
+  'artists.$.work': {
+    type: [Object,],
+    minCount: 1,
+    maxCount: 5,
+  },
+  /*
+   * To be implemented
+  'artists.$.work.$.image': {
+    type: String,
+    autoform: {
+      type: "imageUpload",
+    },
+  },
+  */
+  'artists.$.work.$.workTitle': {
+    type: String,
+  },
+  'artists.$.work.$.medium': {
+    type: String,
+  },
+  'artists.$.work.$.dimensions': {
+    type: String,
+  },
+  'artists.$.work.$.year': {
+    type: Number,
+  },
+});
