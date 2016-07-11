4abcc3d4ee52b843cc24bcef4472b1255fcbf413
Patrick Best <patrickbest@patrickbest.com>
2016-07-12 00:13:40 +0100

    add super ability to admin user & applicant to signups

--- app/imports/api/accounts.js
+++ app/imports/api/accounts.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import { AccountsServer } from 'meteor/accounts-base'
+import { AccountsServer } from 'meteor/accounts-base';
 
 // Creating super admin on application creation
 
@@ -9,9 +9,22 @@ Meteor.startup(() => {
 
     Accounts.createUser({
       email: Meteor.settings.adminEmail,
-      password: Meteor.settings.adminPassword
+      password: Meteor.settings.adminPassword,
     });
+
+    let adminUser = Accounts.findUserByEmail(Meteor.settings.adminEmail);
+
+    Roles.addUsersToRoles(adminUser._id, 'superadmin', Roles.GLOBAL_GROUP);
 
   }
 
+});
+
+// Setting user roles on signup
+
+Accounts.onCreateUser((options, user) => {
+
+  Roles.addUsersToRoles(user._id, ['create-application', 'edit-application', 'submit-application'], 'applicants');
+
+  return user;
 });
