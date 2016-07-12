36ee3d2410345b806ef6267e561890c0af12e1f6
Patrick Best <patrickbest@patrickbest.com>
2016-07-12 23:57:42 +0100

    check role for /apply

--- app/imports/containers/applyContainer.jsx
+++ app/imports/containers/applyContainer.jsx
@@ -7,7 +7,7 @@ import { ApplyLayout } from '/imports/components/apply/applyLayout.jsx';
 import { Applications } from '/imports/collections/applications.js';
 import { ApplicationSections } from '/imports/collections/applicationSections.js';
 
-import { createApplication } from '/imports/api/methods.js';
+import { createApplication } from '/imports/api/methods/applicationMethods.js';
 
 const composer = (props, onData) => {
   // Check if time is after deadline
@@ -26,48 +26,57 @@ const composer = (props, onData) => {
     // Wait for user subscription (kinda)
     if (user) {
 
-      // Look for user's application
-      let application = Applications.findOne({ userId: user._id });
+      // And check roles
+      if (Roles.userIsInRole(user._id, [ 'applicant' ])) {
 
-      if (!!application) {
+        // Look for user's application
+        let application = Applications.findOne({ userId: user._id });
 
-        // Get section URL (props come from the router)
-        const currentSection = props.section;
+        if (!!application) {
 
-        // If section is not in the URL, get the latest position in the application
-        // I changed these conditions. In ES6 you can check for undefined as expected.
-        // Fallback for <IE8 is done by Babel if configured.
-        if (currentSection === undefined && application.position !== undefined) {
-          return FlowRouter.go('/apply/' + application.position);
-        } else if (currentSection === undefined) {
-          // Otherwise go to /apply/1
-          return FlowRouter.go('/apply/1');
+          // Get section URL (props come from the router)
+          const currentSection = props.section;
+
+          // If section is not in the URL, get the latest position in the application
+          // I changed these conditions. In ES6 you can check for undefined as expected.
+          // Fallback for <IE8 is done by Babel if configured.
+          if (currentSection === undefined && application.position !== undefined) {
+            return FlowRouter.go('/apply/' + application.position);
+          } else if (currentSection === undefined) {
+            // Otherwise go to /apply/1
+            return FlowRouter.go('/apply/1');
+          }
+
+          let section = {
+            applicationId: application._id,
+            step: currentSection,
+          };
+
+          let sectionData = ApplicationSections.findOne({
+            applicationId: application._id,
+            step: currentSection,
+          });
+
+          let validated = _.isUndefined(sectionData) || _.isUndefined(sectionData.validated) ? false : sectionData.validated;
+
+          // Merge data
+          section = Object.assign(section, sectionData);
+
+          onData(null, { user, section, application, validated });
+
+        } else {
+          // If no application returned proceed to create a new application for the user
+          createApplication.call({}, (err) => {
+            if (err) {
+              onData(new Meteor.Error(err));
+            }
+          });
         }
 
-        let section = {
-          applicationId: application._id,
-          step: currentSection,
-        };
-
-        let sectionData = ApplicationSections.findOne({
-          applicationId: application._id,
-          step: currentSection,
-        });
-
-        let validated = _.isUndefined(sectionData) || _.isUndefined(sectionData.validated) ? false : sectionData.validated;
-
-        // Merge data
-        section = Object.assign(section, sectionData);
-
-        onData(null, { user, section, application, validated });
-
       } else {
-        // If no application returned proceed to create a new application for the user
-        createApplication.call({}, (err) => {
-          if (err) {
-            onData(new Meteor.Error(err));
-          }
-        });
+        // >>>> How to alert the user to lack of permission
+        console.log('Permission clash needs resolution');
+        onData(null, { user: null });
       }
 
     } else {
