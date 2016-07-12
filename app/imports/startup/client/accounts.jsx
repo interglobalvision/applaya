9f93804612b325cb3fa54bc86455300cf10ab995
Patrick Best <patrickbest@patrickbest.com>
2016-07-12 23:57:31 +0100

    add setupApplicant to signup

--- app/imports/startup/client/accounts.jsx
+++ app/imports/startup/client/accounts.jsx
@@ -1,8 +1,19 @@
 import { Accounts } from 'meteor/std:accounts-ui';
 
+import { setupApplicant } from '/imports/api/methods/accountMethods.js';
+
 Accounts.ui.config({
   passwordSignupFields: 'EMAIL_ONLY',
   loginPath: '/login',
   onSignedInHook: () => FlowRouter.go('/apply'),
   onSignedOutHook: () => FlowRouter.go('/'),
+  onPostSignUpHook: () => {
+    setupApplicant.call({}, (err) => {
+      if (err) {
+        onData(new Meteor.Error(err));
+      } else {
+        FlowRouter.go('/apply')
+      }
+    });
+  }
 });
