387a844361f386108f324c53b1e9f0bac06d9c48
cas <casska@gmail.com>
2016-07-13 14:04:41 -0500

    add actually import accounts.js on server startup

--- app/server/main.js
+++ app/server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
 import '/imports/startup/server/publications.js';
+import '/imports/startup/server/accounts.js';
 
 import '/imports/api/methods/applicationMethods.js';
 import '/imports/api/methods/accountMethods.js';
