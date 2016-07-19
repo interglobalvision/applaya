dc363ab18a14b553bc6be3d342e9874c362d5984
cas <casska@gmail.com>
2016-07-19 17:26:12 -0500

    add downgrading uniforms

--- app/imports/i18n/en.js
+++ app/imports/i18n/en.js
@@ -5,6 +5,11 @@ i18n.addTranslations('en', {
     completed: '{$percentage}% Complete',
     sections: {
       galleryInformation: {
+        title: 'Gallery Information',
+        description: "<p class='flow-text'>Thank you for your application to the third edition of Material Art Fair, which will take place from February 4th - 7th, 2016, at Expo Reforma in Mexico City.</p> <p class='flow-text'>Please remember that the <strong>application deadline is Friday, September 18th, 2015</strong>. No applications will be accepted after that date.</p> \
+        <p class='flow-text'>Each application will be carefully reviewed, considered, and discussed by our 2016 Selection Committee and we will send notification of their decisions to all applicants beginning in early October.</p> \
+        <p class='flow-text'>If at any time you need to pause and continue your application at a later moment, please click the save button so as not to lose any information.</p> \
+        <p class='flow-text'>Please report any errors or problems directly to us at <a href='mailto:applications@material-fair.com' class='u-inline-block'>applications@material-fair.com</a>.</p>",
         galleryName: {
           label: 'Gallery Name'
         },
--- app/imports/i18n/es.js
+++ app/imports/i18n/es.js
@@ -5,6 +5,12 @@ i18n.addTranslations('es', {
     completed: '{$percentage}% Completado',
     sections: {
       galleryInformation: {
+        title: "Información de galería/espacio",
+        description: `<p class='flow-text'>Gracias por tu solicitud a la tercera edición de Material Art Fair, que se llevará a cabo a del 4 al 7 de Febrero del 2016, en el Expo Reforma en la Ciudad de México.</p>
+        <p class='flow-text'>El proceso de registro <strong>concluye el viernes 18 de septiembre del 2015</strong>. No se aceptará ninguna solicitud después de esta fecha.</p>
+        <p class='flow-text'>Cada solicitud será cuidadosamente revisada, considerada y analizada por nuestro comité de selección del 2016. Les haremos llegar la decisión del comité a todos los solicitantes a principios de Octubre.</p>
+        <p class='flow-text'>Si en algún momento necesitas pausar y deseas continuar después con tu solicitud, por favor haz click en el botón “guardar” para no perder tu información.</p>
+        <p class='flow-text'>Por favor reporta cualquier error o problema directamente con nosotros a <a href='mailto:applications@material-fair.com' class='u-inline-block'>applications@material-fair.com</a>.</p>`,
         galleryName: {
           label: 'Nombre de la Galería',
         },
@@ -70,6 +76,7 @@ i18n.addTranslations('es', {
         website: {
           label: 'Sitio web'
         },
+
       },
     },
   },
