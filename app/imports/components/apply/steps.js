6f033d6305811ee828c3207f04146d644f309621
cas <casska@gmail.com>
2016-07-21 17:51:43 -0500

    remove test form from steps

--- app/imports/components/apply/steps.js
+++ app/imports/components/apply/steps.js
@@ -1,5 +1,4 @@
 // Import apply parts
-import { FormTest } from '/imports/components/apply/formTest.jsx';
 import { GalleryInformation } from '/imports/components/apply/galleryInformation.jsx';
 import { ContactInformation } from '/imports/components/apply/contactInformation.jsx';
 import { Proposal } from '/imports/components/apply/proposal.jsx';
@@ -8,7 +7,6 @@ import { Booth } from '/imports/components/apply/booth.jsx';
 import { Terms } from '/imports/components/apply/terms.jsx';
 
 // Import schemas
-import { FormSchema } from '/imports/schemas/testForm.js';
 import { GalleryInformationSchema } from '/imports/schemas/galleryInformation.js';
 import { ContactInformationSchema } from '/imports/schemas/contactInformation.js';
 import { ProposalSchema } from '/imports/schemas/proposal.js';
@@ -17,36 +15,31 @@ import { BoothSchema } from '/imports/schemas/booth.js';
 import { TermsSchema } from '/imports/schemas/terms.js';
 
 export const Steps = [{ // 1
-  component: FormTest,
-  name: 'First Form',
-  key: 'form-test',
-  schema: FormSchema,
-}, { // 2
   component: GalleryInformation,
   name: 'Gallery Information',
   key: 'gallery-information',
   schema: GalleryInformationSchema,
-}, { // 3
+}, { // 2
   component: ContactInformation,
   name: 'Contact Information',
   key: 'contact-information',
   schema: ContactInformationSchema,
-}, { // 4
+}, { // 3
   component: Proposal,
   name: 'Proposal',
   key: 'proposal',
   schema: ProposalSchema,
-}, { // 5
+}, { // 4
   component: Artists,
   name: 'Artists',
   key: 'artists',
   schema: ArtistsSchema,
-}, { // 6
+}, { // 5
   component: Booth,
   name: 'Booth',
   key: 'booth',
   schema: BoothSchema,
-}, { // 7
+}, { // 6
   component: Terms,
   name: 'Terms and conditions',
   key: 'terms-and-conditions',
