a9dbd7ae5e538eae6285ab37e3d0f77e2cf8c3d8
cas <casska@gmail.com>
2016-07-26 22:19:07 -0500

    add custom translatable List, Add and Del fields

--- app/imports/components/fields/listAddField.js
+++ app/imports/components/fields/listAddField.js
@@ -3,21 +3,22 @@ import {connectField}   from 'uniforms';
 import {filterDOMProps} from 'uniforms';
 
 const ListAdd = ({
-    disabled,
-    parent,
-    value,
-    ...props
+  disabled,
+  parent,
+  value,
+  ...props
 }) => {
-    const limitNotReached = !disabled && !(parent.maxCount <= value.length);
+  const limitNotReached = !disabled && !(parent.maxCount <= value.length);
+  const T = i18n.createComponent();
 
-    return (
-        <span
-            {...filterDOMProps(props)}
-            onClick={() => limitNotReached && parent.onChange(parent.value.concat([value]))}
-        >
-            + 
-        </span>
-    );
+  return (
+    <span
+    {...filterDOMProps(props)}
+    onClick={() => limitNotReached && parent.onChange(parent.value.concat([value]))}
+    >
+      <T>fields.list.add</T>
+    </span>
+  );
 };
 
 export default connectField(ListAdd, {includeParent: true, initialValue: false});
--- app/imports/components/fields/listDelField.js
+++ app/imports/components/fields/listDelField.js
@@ -3,25 +3,24 @@ import {connectField}   from 'uniforms';
 import {filterDOMProps} from 'uniforms';
 
 const ListDel = ({
-    disabled,
-    name,
-    parent,
-    ...props
+  disabled,
+  name,
+  parent,
+  label,
+  ...props
 }) => {
-    const fieldIndex      = +name.slice(1 + name.lastIndexOf('.'));
-    const limitNotReached = !disabled && !(parent.minCount >= parent.value.length);
+  const fieldIndex      = +name.slice(1 + name.lastIndexOf('.'));
+  const limitNotReached = !disabled && !(parent.minCount >= parent.value.length);
+  const T = i18n.createComponent();
 
-    return (
-        <span
-            {...filterDOMProps(props)}
-            onClick={() => limitNotReached && parent.onChange([]
-                .concat(parent.value.slice(0,  fieldIndex))
-                .concat(parent.value.slice(1 + fieldIndex))
-            )}
-        >
-            -
-        </span>
-    );
+  return (
+    <span
+    {...filterDOMProps(props)}
+    onClick={() => limitNotReached && parent.onChange([].concat(parent.value.slice(0,  fieldIndex)).concat(parent.value.slice(1 + fieldIndex)))}
+    >
+     <T name={props.field.label}>fields.list.del</T>
+    </span>
+  );
 };
 
 export default connectField(ListDel, {includeParent: true, initialValue: false});
