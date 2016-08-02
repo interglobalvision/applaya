0fbc55990f541b92307847e48efda175051034ae
cas <casska@gmail.com>
2016-08-02 17:06:43 -0500

    add broken pay application method

--- app/imports/api/methods/paymentMethods.js
+++ app/imports/api/methods/paymentMethods.js
@@ -1,11 +1,11 @@
-import { Future } from 'future';
+import Promise from 'promise';
 import { ApplicationPaymentSchema } from '/imports/schemas/applicationPayment.js';
+import Conekta from 'conekta';
 
 // It runs run() only if validate: true.
 export const makePayment = new ValidatedMethod({
   name: 'pay.application',
 
-  // This doesn't receive any arguments, so we don't need validation
   validate: new SimpleSchema({
     amount: {
       type: Number,
@@ -17,7 +17,14 @@ export const makePayment = new ValidatedMethod({
       type: String,
     },
     card: {
-      type: ApplicationPaymentSchema,
+      type: String,
+    },
+    details: {
+      type: new SimpleSchema([ApplicationPaymentSchema, {
+        'card.device_fingerprint': {
+          type: String,
+        },
+      }]),
     },
   }).validator(),
 
@@ -31,49 +38,52 @@ export const makePayment = new ValidatedMethod({
 
     Conekta.api_key = Meteor.settings.conekta_private;
 
-    data.email = Meteor.user().emails[0].address;
+    //data.email = Meteor.user().emails[0].address;
 
     debugger;
     let conektaSync = (data) => {
-      let future = new Future();
 
-      debugger;
-      Conekta.Charge.create({
-        "amount": data.amount,
-        "currency": "USD",
-        "description": "Material Art Fair",
-        //"reference_id": data.applicationId,
-        "card": data.token,
-        "details": {
-          //"email": data.email,
-          "line_items": [{
-          "name": "Material Art Fair application fee",
-          "sku": "maf_fee_1",
-          "unit_price": 12500,
-          "description": "Art Fair Application",
-          "quantity": 1,
-          "type": "application",
-        },],
-          "phone": data.tel,
-          "billing_address": {
-            "street1": data.address1,
-            "street2": data.address2,
-            "city": data.city,
-            "state": data.state,
-            "zip": data.postalCode,
-            "country": data.country,
+      return new Promise((resolve, reject) => {
+        Conekta.Charge.create({
+          "amount": data.amount,
+          "currency": "USD",
+          "description": "Material Art Fair",
+          //"reference_id": data.applicationId,
+          "card": data.card,
+          "details": {
+            //"email": data.email,
+            "line_items": [{
+              "name": "Material Art Fair application fee",
+              "sku": "maf_fee_1",
+              "unit_price": 12500,
+              "description": "Art Fair Application",
+              "quantity": 1,
+              "type": "application",
+            },],
+            "billing_address": {
+              "street1": data.details.street1,
+              "street2": data.details.street2,
+              "city": data.details.city,
+              "state": data.details.state,
+              "zip": data.details.zip,
+              "country": data.details.country,
+              "phone": data.phone,
+              "cellphone": data.cellphone,
+            },
           },
-        },
-      },
-        charge => future.return(charge),
-        error => future.return(error),
-      );
+        }, (err, res) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(charge);
+            }
+          }
+        );
 
-      return future.wait();
+      });
     };
 
-    let chargeResult = conektaSync(data);
-    debugger;
+    let chargeResult = await conektaSync(data);
 
     console.log('New transaction: ', chargeResult);
 
