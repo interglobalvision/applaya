0ab9ece9a31a078b92f0c2b424c7bf677012d420
Patrick Best <patrickbest@patrickbest.com>
2016-08-04 15:55:17 -0500

    add intro text to /apply/introduction

--- app/imports/components/apply/galleryInformation.jsx
+++ app/imports/components/apply/galleryInformation.jsx
@@ -16,7 +16,6 @@ export class GalleryInformation extends ApplySection {
     return (
       <section>
         <h2><T>apply.sections.galleryInformation.title</T></h2>
-        <T>apply.sections.galleryInformation.description</T>
         <AutoForm
           autosave
           schema={GalleryInformationSchema}
