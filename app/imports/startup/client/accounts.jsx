ab932972d81f4d655de33492e2b981a1f735c604
Patrick Best <patrickbest@patrickbest.com>
2016-08-05 19:59:28 -0500

    set lang pref on signup and load pref on login

--- app/imports/startup/client/accounts.jsx
+++ app/imports/startup/client/accounts.jsx
@@ -8,6 +8,11 @@ Accounts.ui.config({
   onSignedInHook: () => {
     const user = Meteor.user();
 
+    // set active locale to user lang pref
+    if (!_.isUndefined(user.profile.lang)) {
+      i18n.setLocale(user.profile.lang);
+    }
+
     if (Roles.userIsInRole(user._id, [ 'applicant' ])) {
       FlowRouter.go('/apply');
     } else if (Roles.userIsInRole(user._id, [ 'admin' ])) {
@@ -16,9 +21,14 @@ Accounts.ui.config({
   },
   onSignedOutHook: () => FlowRouter.go('/'),
   onPostSignUpHook: () => {
+    // set lang pref in user.profile
+    let lang = i18n.getLocale();
+    Meteor.users.update(Meteor.userId(), { $set: {'profile.lang': args.lang} });
+
     setupApplicant.call({}, (err) => {
       if (err) {
-        onData(new Meteor.Error(err));
+        console.log(err);
+        new Meteor.Error(err);
       } else {
         FlowRouter.go('/apply')
       }
