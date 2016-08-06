03ab95ee14f5bb5cb87c3c093ade3acd3e6934f0
cas <cas@interglobal.vision>
2016-08-06 17:24:07 -0500

    fix payment notifications

--- app/imports/components/payment/PaymentLayout.jsx
+++ app/imports/components/payment/PaymentLayout.jsx
@@ -45,7 +45,7 @@ export class PaymentLayout extends Component {
           return console.log(err);
         }
 
-        Alert.success(i18n.__('notifications.payment.success');
+        Alert.success(i18n.__('notifications.payment.success'));
         FlowRouter.go('/apply/thanks');
 
       });
