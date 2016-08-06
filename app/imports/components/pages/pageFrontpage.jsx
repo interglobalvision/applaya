663d11ce3e87c21675e6ef896ce068c89aa225a7
Patrick Best <patrickbest@patrickbest.com>
2016-08-05 20:12:51 -0500

    add frontpage routing to signup with locale set

---
+++ app/imports/components/pages/pageFrontpage.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import i18n from 'meteor/universe:i18n';
+
+export class PageFrontpage extends Component {
+  onLanguageEn() {
+    i18n.setLocale('en');
+    FlowRouter.go('/signup');
+  }
+
+  onLanguageEs() {
+    i18n.setLocale('es');
+    FlowRouter.go('/signup');
+  }
+
+  render() {
+    const T = i18n.createComponent();
+
+    return(
+      <div className="row">
+        <section className="fluid-col s-12">
+          <h1><T>frontpage.title</T></h1>
+          <T>frontpage.introduction</T>
+          <a className='button' onClick={this.onLanguageEn}>English</a>
+          <a className='button' onClick={this.onLanguageEs}>Español</a>
+        </section>
+      </div>
+    );
+  }
+};
