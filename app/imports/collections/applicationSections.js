4201bc4314b38a8ff0dd83bc9ccceb1bdf38ea56
cas <cas@interglobal.vision>
2016-08-08 12:58:18 -0500

    fix collections update to be compatible with Mongol

--- app/imports/collections/applicationSections.js
+++ app/imports/collections/applicationSections.js
@@ -13,7 +13,14 @@ class ApplicationSectionsCollection extends Mongo.Collection {
   }
 
   update(selector, modifier, ...optionsAndCallback) {
-    modifier.$set['updatedAt'] = modifier.$set['updatedAt'] || new Date();
+    
+    // When modifying whole document (ex. Mongol)
+    if(_.isUndefined(modifier.$set)) {
+      modifier['updatedAt'] = modifier['updatedAt'] || new Date();
+    } else if (!_.isUndefined(modifier.$set)) {
+    // When modifying whole document (ex. Mongol)
+      modifier.$set['updatedAt'] = modifier.$set['updatedAt'] || new Date();
+    }
   
     return super.update(selector, modifier, ...optionsAndCallback);
   }
