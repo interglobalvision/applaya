f6202e1f27fcc1516a7ce55b207c3c2b82193834
Patrick Best <patrickbest@patrickbest.com>
2016-08-09 14:35:44 -0500

    conditional routing to home

--- app/imports/components/pages/pageFrontpage.jsx
+++ app/imports/components/pages/pageFrontpage.jsx
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import i18n from 'meteor/universe:i18n';
 
 export class PageFrontpage extends Component {
+  componentWillMount() {
+    // Conditional routing for logged in users
+    if (Meteor.userId()) {
+      if (Roles.userIsInRole(Meteor.userId(), 'applicant')) {
+        FlowRouter.go('/apply');
+      } else if (Roles.userIsInRole(Meteor.userId(), 'committee')) {
+        FlowRouter.go('/applications');
+      } else if (Roles.userIsInRole(Meteor.userId(), 'admin')) {
+        FlowRouter.go('/admin');
+      }
+    }
+  }
+
   onLanguageEn() {
     i18n.setLocale('en');
     FlowRouter.go('/signup');
